9bd834a802e0c2adb04617e18895c28f131f26ae
FLX01 <don_fl@gmx.de>
2017-09-05 15:14:07 +0200

    Projektdateien hinzufügen.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Hallo = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Hallo
+        '
+        Me.Hallo.AllowDrop = True
+        Me.Hallo.AutoSize = True
+        Me.Hallo.Location = New System.Drawing.Point(101, 108)
+        Me.Hallo.Name = "Hallo"
+        Me.Hallo.Size = New System.Drawing.Size(39, 13)
+        Me.Hallo.TabIndex = 0
+        Me.Hallo.Text = "Ja Klar"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(94, 158)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(204, 267)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Hallo)
+        Me.Name = "Form1"
+        Me.Text = "Form133"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Hallo As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+
+
+
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+
+    Private Sub NewMethod() Handles Button1.Click
+
+        MessageBox.Show("Moin")
+        Hallo.Text = "Moin"
+
+
+    End Sub
+End Class
